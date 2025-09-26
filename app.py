43ef7a43e27e3222441a2be75da181439125e872
shan-771 <your_email@example.com>
2025-09-27 00:33:26 +0530

    Initial commit - Smart Food Donation project

--- app.py
+++ app.py
@@ -3,17 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('login.html')  # Your main page
+def home():  # main login page
+    return render_template('Home.html')
+
+@app.route('/login')
+def login():  # main login page
+    return render_template('login.html')
 
 @app.route('/signup')
-def login():
-    return render_template('signup.html')  # Example login page
+def signup():
+    return render_template('signup.html')
+
 
 @app.route('/restaurant')
 def restaurant():
-    return render_template('restaurant.html')  # Restaurant donation page
+    return render_template('restaurant.html')
 
 @app.route('/ngo')
 def ngo():
-    return render_template('ngo.html')  # NGO dashboard page
+    return render_template('ngo.html')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
